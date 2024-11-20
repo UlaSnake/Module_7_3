5824f486c7e47cef496ddc3a516b3a4d072fbef7
UlaSnake <uliyatender@yandex.ru>
2024-11-20 16:29:08 +0300

    Add files via upload

--- Module_7_3.py
+++ Module_7_3.py
@@ -1,5 +1,6 @@
 import string
 
+
 class WordsFinder:
     def __init__(self, *file_names):
         # Сохраняем названия файлов как кортеж
@@ -9,15 +10,22 @@ class WordsFinder:
         """Возвращает словарь с названиями файлов и списками слов из них."""
         all_words = {}  # Создаем пустой словарь для хранения результатов
 
+        # Задаем список символов для удаления
+        chars_to_remove = list(string.punctuation + ' -')  # Пунктуация и пробелы
+
         for file_name in self.file_names:  # Перебираем названия файлов
             try:
                 with open(file_name, 'r', encoding='utf-8') as file:  # Открываем файл для чтения
-                    # Читаем содержимое файла, переводим в нижний регистр
+                    # Читаем содержимое файла и переводим в нижний регистр
                     text = file.read().lower()
-                    # Удаляем пунктуацию из текста
-                    text = text.translate(str.maketrans('', '', string.punctuation + ' -'))
+
+                    # Удаляем нежелательные символы
+                    for char in chars_to_remove:
+                        text = text.replace(char, " ")  # Заменяем каждый символ на пробел
+
                     # Разбиваем текст на слова по пробелам
-                    words = text.split()
+                    words = text.split()  # Разбиваем текст на слова по пробелам
+
                     # Записываем в словарь: имя файла - список слов
                     all_words[file_name] = words
 
@@ -33,7 +41,7 @@ class WordsFinder:
         for file_name, words in self.get_all_words().items():  # Получаем все слова из файлов
             if word.lower() in words:  # Проверяем наличие слова (в нижнем регистре)
                 position = words.index(word.lower())  # Находим позицию первого вхождения слова
-                word_positions[file_name] = position + 1  # Сохраняем позицию (с учетом индексации с 1)
+                word_positions[file_name] = position  # Сохраняем позицию
 
         return word_positions  # Возвращаем словарь с позициями
 
@@ -51,7 +59,7 @@ class WordsFinder:
 
 # Создаем тестовый файл test_file.txt с примером текста
 with open('test_file.txt', 'w', encoding='utf-8') as f:
-    f.write("It's a text for task Найти везде.\n" "Используйте его для самопроверки.\n" "Успехов в решении задачи!\n")
+    f.write("It's a text for task Найти везде. Используйте его для самопроверки. Успехов в решении задачи!\n")
     f.write("text text text\n")
 
 # Пример использования класса
@@ -62,7 +70,8 @@ print(finder2.get_all_words())
 # Ожидаемый вывод: {'test_file.txt': ["it's", 'a', 'text', 'for', 'task', 'найти',
 #                                    'везде', 'используйте', 'его', 'для',
 #                                    'самопроверки', 'успехов', 'в',
-#                                    'решении', 'задачи!', 'text', 'text', 'text']}
+#                                    'решении', 'задачи!',
+#                                    'text', 'text', 'text']}
 
 # Ищем слово 'TEXT' и выводим его позицию
 print(finder2.find('TEXT'))
